2e37cdbc803c8ad54eae3786a12d502a779c40ed
Aniketrana <ranaannu93@gmail.com>
2020-07-21 16:25:31 +0530

    Add project files.

---
+++ CustomValidation/Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomValidation.Models
+{
+    public class Emp
+    {
+        [Required]
+        public string Name { get; set; }
+        [aniketValidation]
+        public string Message { get; set; }
+    }
+}
---
+++ CustomValidation/aniketValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CustomValidation
+{
+    public class aniketValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string msg = value.ToString();
+                if (msg.Contains("Aniket"))
+                {
+                    return ValidationResult.Success;
+                }                
+            }
+            ErrorMessage = ErrorMessage ?? validationContext.DisplayName + " Field must contain Aniket";
+            // ?? operator use for checking null value or not
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
