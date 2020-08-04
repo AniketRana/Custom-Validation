2ab4a9741c94833a4265fd6e8b06ddde7bb2cd7f
Aniketrana <ranaannu93@gmail.com>
2020-08-04 12:48:40 +0530

    04-08-2020

---
+++ DependencyInjection/BL/Custom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    class Custom : IEmployee
+    {
+        public int GetToalEmp()
+        {
+            return 100;
+        }
+    }
+}
---
+++ DependencyInjection/BL/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    public class Employee : IEmployee
+    {
+
+        public int GetToalEmp()
+        {
+            return 20;
+        }
+
+    }
+}
